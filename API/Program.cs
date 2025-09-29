85318024246d57e75c037da7a341e20edfc93daf
Aiza <aizablocanoy@gmail.com>
2025-09-30 08:38:44 +1300

    Keep basic codes

--- API/Program.cs
+++ API/Program.cs
@@ -4,25 +4,10 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 
-builder.Services.AddOpenApi(); //This replaced the Swagger in .NET 9.0 
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-
  var app = builder.Build();
 // These are middlewares
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.MapOpenApi();
-    app.UseSwagger();
-    app.UseSwaggerUI(); 
-
-}
-
-app.UseHttpsRedirection();
-
-app.UseAuthorization();
 
 app.MapControllers();
 

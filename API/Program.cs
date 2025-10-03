74474c5cc4a9bb5677dec0cfe31cb94852a49f84
Aiza <aizablocanoy@gmail.com>
2025-10-03 23:26:32 +1300

    Add API project with EF code

--- API/Program.cs
+++ API/Program.cs
@@ -1,10 +1,19 @@
+using Infrastructure.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+
 builder.Services.AddControllers();
+builder.Services.AddDbContext<StoreContext>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
+
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 
- var app = builder.Build();
+var app = builder.Build();
 // These are middlewares
 
 // Configure the HTTP request pipeline.
